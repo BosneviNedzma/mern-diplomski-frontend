4ca892fb72a7ef0961dd7866bebd4c0e20e759b8
Nedzma Brkic <brkicnedzma@gmail.com>
2024-06-13 22:57:45 +0200

    Update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,15 +6,15 @@ const HomePage = () => {
         <div className="flex flex-col gap-12">
             <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
                 <h1 className="text-5xl font-bold tracking-tight text-green-600">
-                    Budi zdraviji već danas
+                    Organska dobrota na dlanu
                 </h1>
                 <span className="text-xl">Klik te dijeli od sreće</span>
             </div>
             <div className="grid md:grid-cols-2 gap-5">
                 <img src={landingImage}  />
                 <div className="flex flex-col items-center justify-center gap-4 text-center">
-                    <span className="font-bold text-3xl tracking-tighter">Sa sela do Vašeg stola!</span>
-                    <span>Naše, a valja.</span>
+                    <span className="font-bold text-3xl tracking-tighter">Sa sela do vašeg stola!</span>
+                    <span>bosanskohercegovačka proizvodnja</span>
                     <img src={appDownloadImage} />
                 </div>
             </div>
